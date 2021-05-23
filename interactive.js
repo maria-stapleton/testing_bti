510593061dedcc883a4c8c2a6f9df82d2ca68ad3
maria-stapleton <maria.stapleton@unilever.com>
2021-05-24 00:51:14 +0100

    verynew

--- interactive.js
+++ interactive.js
@@ -1,20 +1,32 @@
-// SMOOTH SCROLLING SECTIONS
-$("a[href*=#]:not([href=#])").click(function () {
-  if (
-    location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") ||
-    location.hostname == this.hostname
-  ) {
-    var target = $(this.hash);
-    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-    if (target.length) {
-      $("html,body").animate(
-        {
-          scrollTop: target.offset().top
-        },
-        1000
-      );
-      return false;
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+let mainSections = document.querySelectorAll("main section");
+
+let lastId;
+let cur = [];
+
+// This should probably be throttled.
+// Especially because it triggers during smooth scrolling.
+// https://lodash.com/docs/4.17.10#throttle
+// You could do like...
+// window.addEventListener("scroll", () => {
+//    _.throttle(doThatStuff, 100);
+// });
+// Only not doing it here to keep this Pen dependency-free.
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
     }
-  }
+  });
 });
 
